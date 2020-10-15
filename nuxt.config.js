09206c8c569818f5977135a6741d00e941ee79f9
unkown <fred@ocdigitals.nl>
2020-10-15 22:28:49 +0200

    add coming soon site

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,41 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Numago - Internetbureau | Software Development, IT Consultancy, Online Marketing',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    // <link rel="manifest" href="/site.webmanifest"></link>
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: 'favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '64x64',
+        href: '/favicon-64x64.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,31 @@
     <!-- <div class="flex justify-center"></div> -->
     <div class="flex justify-center">
       <div class="max-w-sm rounded overflow-hidden shadow-lg mt-20 bg-white">
-        <div class="px-6 py-4">
-          <img
-            src="~/assets/images/numago-logo.svg"
-            alt=""
-            class="sm:mt-15 h-20"
-          />
+        <div class="px-6 py-4 flex flex-wrap justify-center">
+          <img src="~/assets/images/numago-logo.svg" alt="" class="mt-4 h-20" />
           <p class="text-gray-700 text-base mt-4">
-            Software Development, IT Consultancy, Online Marketing
+            <b>Numago - Internetbureau</b> <br />
+            <i>Software Development, IT Consultancy, Online Marketing</i>
           </p>
-          <ul class="mt-4">
-            <li>E-mail: <a href="mailto:info@numago.nl">info@numago.nl</a></li>
-            <li>KVK: 76360601</li>
-          </ul>
-          <div class="font-bold text-xl mb-2 mt-10 text-center">
-            Coming soon..
+          <div class="w-full">
+            <ul class="mt-4">
+              <li class="">
+                E-mail:
+                <a :href="`mailto:${emailFst + emailLst}`">{{
+                  emailFst + emailLst
+                }}</a>
+              </li>
+              <li>KVK: 76360601</li>
+            </ul>
           </div>
+          <!-- <div class="font-bold text-xl mb-2 mt-10 text-center">
+            Binnekort online..
+          </div> -->
+        </div>
+        <div
+          class="bg-black text-white font-bold text-xl py-3 mt-4 text-center"
+        >
+          Binnekort online..
         </div>
       </div>
     </div>
@@ -32,6 +41,18 @@
     <!-- </div> -->
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      emailFst: 'info',
+      emailLst: '@numago.nl',
+    }
+  },
+})
+</script>
 
 <style lang="postcss" scoped>
 /* Sample `apply` at-rules with Tailwind CSS
